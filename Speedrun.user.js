6db03966692a3bc25ebdc47c5f5d0c4b26a2361d
perpil <97474956+perpil@users.noreply.github.com>
2022-06-13 10:51:26 -0700

    Add magic copy
    Pulse button on deeplink
    Switch to hidden on snackbar

--- Speedrun.user.js
+++ Speedrun.user.js
@@ -534,7 +534,14 @@ input:checked + .slider:before {
 .slider.round:before {
   border-radius: 50%;
 }
-        </style>`);
+.copyCursor {
+    cursor: copy;
+}
+
+.copyCursor:hover {
+  color:var(--color-accent-fg);
+}
+</style>`);
 
     //let toolbar = $('<div/>',{"css":{"position":"fixed","top":"0","right":"0","padding":"2px","z-index":"50","border-radius":"5px", "background": "rgba(0, 0, 0, .6)"}});
     //let toolbar = $('<div/>',{"id":"srToolbar","css":{"display":"none", "position":"fixed","top":"0","left": "50%","transform":"translate(-50%, 0)","padding":"2px","z-index":"50","border-radius":"5px", "background": "var(--color-header-bg)"}});
@@ -1466,7 +1473,8 @@ input:checked + .slider:before {
     $(document).ready(function() {
         $('#service,#region').select2({
             dropdownAutoWidth : true,
-            width:'copy'});
+            width:'copy'
+        });
         if(window.location.hash) {
             const el = document.getElementById('user-content-' + window.location.hash.substring(1));
             if(el) {
@@ -1545,6 +1553,10 @@ input:checked + .slider:before {
         }
 
         serviceDropdown.trigger('change');
+
+        for(const block of $("p > code")) {
+            $(block).after(`<span class='copyCursor'><clipboard-copy aria-label="Copy text" value="${$(block).text()}" data-view-component="true" tabindex="0" role="button">    <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy" style="display: inline-block;">    <path fill-rule="evenodd" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill-rule="evenodd" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path></svg>    <svg style="display: none;" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success">    <path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg></clipboard-copy></span>`);
+        }
 
     }
 
