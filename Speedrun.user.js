29607f272815b40e1e9981d44c43f11e8f2b53ed
perpil <97474956+perpil@users.noreply.github.com>
2024-01-31 17:05:53 -0800

    Fix issue with function cache

--- Speedrun.user.js
+++ Speedrun.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Speedrun
 // @namespace    https://speedrun.nobackspacecrew.com/
-// @version      1.103
+// @version      1.103.1
 // @description  Table Flip Dev Ops
 // @author       No Backspace Crew
 // @require      https://speedrun.nobackspacecrew.com/js/jquery@3.7.0/jquery-3.7.0.min.js
@@ -1528,10 +1528,11 @@ async function interpolate(tpl, variables, suppressErrors, throwErrors=true) {
         //        keys.delete
         //fn = new Function(...keys,'return `' + tpl.replace(/`/g, '\\`') + '`;'); //not sure why nested template literals are prevented
         let isAsync = tpl.includes('await ');
-        let fn = functionCache[tpl];
+        let cacheKey = tpl+keys;
+        let fn = functionCache[cacheKey];
         if(!fn) {
             fn = isAsync ? new AsyncFunction(...keys,`return \`${tpl}\``) : new Function(...keys,`return \`${tpl}\``);
-            functionCache[tpl] = fn;
+            functionCache[cacheKey] = fn;
         }
         let result = isAsync ? await fn(...keys.map(x => variables[x])) : fn(...keys.map(x => variables[x]));
         if(!suppressErrors && hasDOMContent(result)){
@@ -3160,6 +3161,5 @@ function srUpdateClickHandler(event) {
     srUpdate.attr('title',`Update page to Speedrun V.${newSRVersion}`);
     srUpdate.find('svg').replaceWith('<svg class="octicon octicon-tools color-fg-attention" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20"><path d="M1.705 8.005a.75.75 0 0 1 .834.656 5.5 5.5 0 0 0 9.592 2.97l-1.204-1.204a.25.25 0 0 1 .177-.427h3.646a.25.25 0 0 1 .25.25v3.646a.25.25 0 0 1-.427.177l-1.38-1.38A7.002 7.002 0 0 1 1.05 8.84a.75.75 0 0 1 .656-.834ZM8 2.5a5.487 5.487 0 0 0-4.131 1.869l1.204 1.204A.25.25 0 0 1 4.896 6H1.25A.25.25 0 0 1 1 5.75V2.104a.25.25 0 0 1 .427-.177l1.38 1.38A7.002 7.002 0 0 1 14.95 7.16a.75.75 0 0 1-1.49.178A5.5 5.5 0 0 0 8 2.5Z"></path></svg>');
 }
-
-    //  ]]></>).toString(), { "targets": "> 0.25%, not dead"}).code);
+//  ]]></>).toString(), { "targets": "> 0.25%, not dead"}).code);
 })();
